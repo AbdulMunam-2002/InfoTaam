05d855293fc6e857c52364938825b9246760f032
AbdulMunam-2002 <abdulmunam666@gmail.com>
2024-07-16 05:09:14 +0500

    Drinks update

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import Home from './Components/Home';
 import Breakfast from './Components/Breakfast';
+import Drinks from './Components/Drinks';
 
 function App() {
   return (
     <>
       <Home/>
       <Breakfast/>
+      <Drinks/>
     </>
   );
 }
